a5a67161dea53153fde3ac3ac2f3b4d6c3aeea65
piyushvj <er.piyushvj@gmail.com>
2025-08-30 16:14:14 +0530

    Initial commit

---
+++ src/main/java/com/piyush/geni/GeniApplication.java
@@ -0,0 +1,13 @@
+package com.piyush.geni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piyush/geni/GeniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyush.geni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
